299ddf1636510d6b4a497d36ecfe24a3893a4a56
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-14 11:03:22 +0800

    add parking car,fectch car and fetch multiple parking,fetch multiple car

---
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ParkingBoyTest {
+    @Test
+     void should_return_car_when_fetch_car_given_car_ticket(){
+         //given
+         ParkingLot parkingLot=new ParkingLot();
+         ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+         Car car=new Car();
+         Ticket ticket=parkingBoy.park(car);
+         //when
+         Car fetchedCar=parkingBoy.fectch(ticket);
+         //then
+         Assertions.assertSame(car, fetchedCar);
+     }
+     @Test
+     void should_return_car_when_fetch_car_given_ticket(){
+         //given
+         ParkingLot parkingLot=new ParkingLot();
+         ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+         Car carone=new Car();
+         Car cartwo=new Car();
+         Ticket ticketone=parkingBoy.park(carone);
+         Ticket tickettwo=parkingBoy.park(cartwo);
+         //when
+         Car fetchedCarone=parkingBoy.fectch(ticketone);
+         Car fetchedCartwo=parkingBoy.fectch(tickettwo);
+         //then
+         Assertions.assertSame(carone, fetchedCarone);
+         Assertions.assertSame(cartwo, fetchedCartwo);
+     }
+
+
+}
