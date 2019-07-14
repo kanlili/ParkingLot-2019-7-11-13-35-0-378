0131366b5b09273b85cd43f21655f2f2f271db51
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-14 11:06:23 +0800

    add test parking car,fectch car and fetch multiple parking,fetch multiple car

--- src/main/java/com/thoughtworks/tdd/ParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/ParkingBoy.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.tdd;
 
 public class ParkingBoy {
+    private ParkingLot parkingLot;
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public Ticket park(Car car) {
+        Ticket ticket=new Ticket();
+        parkingLot.park(ticket,car);
+        return ticket;
+    }
+
+    public Car fectch(Ticket ticket) {
+        return  parkingLot.getCar(ticket);
+    }
 }
--- src/main/java/com/thoughtworks/tdd/ParkingLot.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLot.java
@@ -1,4 +1,15 @@
 package com.thoughtworks.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private Map<Ticket,Car> cars=new HashMap<>();
+    public  void park(Ticket ticket, Car car) {
+        cars.put(ticket, car);
+    }
+
+    public Car getCar(Ticket ticket) {
+       return  cars.get(ticket);
+    }
 }
